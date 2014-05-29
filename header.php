416f77a72e3a089448b8184876c362bf579c3482
romaincoeur <coeurro@gmail.com>
2014-05-29 11:02:49 +0200

    nouveau fichier: assets/style.css 	nouveau fichier: favicon.ico 	nouveau fichier: footer.php 	nouveau fichier: header.php 	nouveau fichier: images/Logos_partenaires.jpg 	nouveau fichier: images/logo.png 	nouveau fichier: images/paysage_st_victor_de_buthon_long_v3.jpg 	nouveau fichier: index.php 	nouveau fichier: robots.txt

---
+++ header.php
@@ -0,0 +1,217 @@
+
+
+
+<head>
+
+    <base href="">
+    <meta charset="utf-8" />
+    <title>Simplon-village</title>
+    <meta itemprop="thumbnailUrl" content="http://static.squarespace.com/static/523ee688e4b0534df7deb0be/t/52420b93e4b0169e0fba9c09/1400828281994/?format=1000w"/>
+    <link rel="image_src" href="http://static.squarespace.com/static/523ee688e4b0534df7deb0be/t/52420b93e4b0169e0fba9c09/1400828281994/?format=1000w" />
+    <meta name="description" content="Fabrique de codeurs entrepreneurs à Montreuil #FrenchTech" />
+    <script type="text/javascript">SQUARESPACE_ROLLUPS = {};</script><script>(function(rollups, name) { if (!rollups[name]) { rollups[name] = {}; } rollups[name].js = ["//static.squarespace.com/universal/scripts-compressed/common-8871bdbf28c24741fa1ad95bb7c004fd-min.js"]; })(SQUARESPACE_ROLLUPS, 'squarespace-common');</script>
+    <script>(function(rollups, name) { if (!rollups[name]) { rollups[name] = {}; } rollups[name].js = ["//static.squarespace.com/universal/scripts-compressed/commerce-abac10f64b2bf33e97b28f2b7e9962fa-min.js"]; })(SQUARESPACE_ROLLUPS, 'squarespace-commerce');</script>
+    <script>(function(rollups, name) { if (!rollups[name]) { rollups[name] = {}; } rollups[name].css = ["//static.squarespace.com/universal/styles-compressed/commerce-6bd2c698524ea399edbd32b618e558d6-min.css"]; })(SQUARESPACE_ROLLUPS, 'squarespace-commerce');</script>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+    <script type="text/javascript" data-sqs-type="dynamic-assets-loader">(function() {(function(a){try{if(window.top!=window&&window.top.Squarespace&&window.top.Squarespace.frameAvailable){window.top.Squarespace.frameAvailable(window,SQUARESPACE_ROLLUPS);return}}catch(d){}for(var c in a){if(a[c].js)for(var b=0;b<a[c].js.length;b++)document.write('<script type="text/javascript" src="'+a[c].js[b]+'">\x3c/script>');if(a[c].css)for(b=0;b<a[c].css.length;b++)document.write('<link rel="stylesheet" type="text/css" href="'+a[c].css[b]+'" />')}})(SQUARESPACE_ROLLUPS);
+        })();</script>
+    <script>
+        Static.SQUARESPACE_CONTEXT =
+        {"templateTweakable":true,
+            "website":
+            {"id":"523ee688e4b0534df7deb0be",
+                "identifier":"simplonco",
+                "contentModifiedOn":1400828281994,
+                "cloneable":false,
+                "siteStatus":{},
+                "language":"fr-FR",
+                "timeZone":"Europe/Paris",
+                "machineTimeZoneOffset":7200000,
+                "timeZoneOffset":7200000,
+                "timeZoneAbbr":"CEST",
+                "siteTitle":"Simplon.co",
+                "siteTagLine":"Fabrique de codeurs entrepreneurs \u00E0 Montreuil #frenchtech","siteDescription":"<p>Proud member of \"la French Tech\" et entreprise agr\u00E9\u00E9e solidaire par l'Etat, Simplon.co est une fabrique acc\u00E9l\u00E9r\u00E9e de d\u00E9veloppeurs d'applications web/mobile et d'entrepreneurs sociaux du num\u00E9rique prioritairement ouverte aux jeunes des quartiers populaires, des milieux modestes, aux filles et aux porteurs de projets innovants tourn\u00E9s vers les activit\u00E9s d'int\u00E9r\u00EAt g\u00E9n\u00E9ral (sant\u00E9, social, d\u00E9pendance, environnement, \u00E9ducation, \u00E9nergies, transports\u2026) et l'innovation sociale (ESS, entrepreneuriat social). Cr\u00E9\u00E9e \u00E0 Montreuil (93), l'initiative Simplon.co est actuellement en cours d'essaimage en France et \u00E0 l'\u00E9tranger et a d'ores et d\u00E9j\u00E0 ouvert depuis janvier 2014 une promotion d\u00E9tach\u00E9e dans la cit\u00E9 sensible de La Noue \u00E0 Villeneuve la Garenne (92) et une filiale \u00E0 Cluj en Roumanie. En moins d\u2019un an, ce sont 50 demandeurs d\u2019emploi ou allocataire du RSA issues des quartiers populaires, de la diversit\u00E9 et de milieux modestes, une centaine de salari\u00E9s, plus de 250 enfants \u00E0 la programmation informatique et a permis \u00E0 une dizaine de projets d\u2019\u00EAtre incub\u00E9s ou lanc\u00E9s.</p>",
+                "location":{"mapZoom":12.0,"mapLat":48.854552,"mapLng":2.435783399999991,"markerLat":48.854552,"markerLng":2.435783399999991,"addressTitle":"Simplon.co","addressLine1":"55 rue de Vincennes","addressLine2":"Montreuil","addressCountry":"France"},"logoImageId":"52420b93e4b0169e0fba9c09","shareButtonOptions":{"2":true,"1":true,"3":true,"4":true},"logoImageUrl":"//static.squarespace.com/static/523ee688e4b0534df7deb0be/t/52420b93e4b0169e0fba9c09/1400828281994/","authenticUrl":"http://simplon.co","baseUrl":"http://simplon.co","primaryDomain":"simplon.co","socialAccounts":[{"serviceId":2,"userId":"637490886","userName":"andrei.volt","screenname":"Simplon.co","addedOn":1379854468385,"profileUrl":"http://www.facebook.com/Simplon.co","iconUrl":"http://graph.facebook.com/637490886/picture?type=square","metaData":{"service":"facebook"},"iconEnabled":true,"serviceName":"facebook"},{"serviceId":4,"userId":"1374547314","userName":"simplonco","screenname":"Simplon.co","addedOn":1379930728028,"profileUrl":"https://twitter.com/simplonco","iconUrl":"http://a0.twimg.com/profile_images/378800000016910707/80f09b042a8c2a2b53f06b391ab63ab4_normal.png","collectionId":"52401268e4b0b7dd2fafc4bb","iconEnabled":true,"serviceName":"twitter"},{"serviceId":4,"userId":"1374547314","userName":"simplonco","screenname":"Simplon.co","addedOn":1379930740612,"profileUrl":"https://twitter.com/simplonco","iconUrl":"http://a0.twimg.com/profile_images/378800000016910707/80f09b042a8c2a2b53f06b391ab63ab4_normal.png",
+                "collectionId":"52401274e4b0496cd9c8f22e",
+                "iconEnabled":true,"serviceName":"twitter"
+            },
+                {"serviceId":2,
+                    "userId":"627562597",
+                    "userName":"frederic.bardeau",
+                    "screenname":"Fr\u00E9d\u00E9ric Bardeau",
+                    "addedOn":1379977987928,
+                    "profileUrl":"http://www.facebook.com/627562597",
+                    "iconUrl":"http://graph.facebook.com/627562597/picture?type=square",
+                    "metaData":{"service":"facebook"},
+                    "iconEnabled":true,
+                    "serviceName":"facebook"
+                },
+                {"serviceId":2,
+                    "userId":"627562597",
+                    "userName":"frederic.bardeau",
+                    "screenname":"Fr\u00E9d\u00E9ric Bardeau",
+                    "addedOn":1380058872633,
+                    "profileUrl":"http://www.facebook.com/627562597",
+                    "iconUrl":"http://graph.facebook.com/627562597/picture?type=square",
+                    "metaData":{"service":"facebook"},
+                    "iconEnabled":true,
+                    "serviceName":"facebook"
+                },
+                {"serviceId":23,
+                    "userId":"65222",
+                    "userName":"andreivolt",
+                    "screenname":"Andrei Vladescu-Olt",
+                    "addedOn":1396739850586,
+                    "profileUrl":"https://github.com/andreivolt",
+                    "iconUrl":"https://avatars.githubusercontent.com/u/65222?",
+                    "iconEnabled":true,
+                    "serviceName":"github"
+                },
+                {"serviceId":23,
+                    "userId":"65222",
+                    "userName":"andreivolt",
+                    "screenname":"Andrei Vladescu-Olt",
+                    "addedOn":1396739953966,
+                    "profileUrl":"https://github.com/andreivolt",
+                    "iconUrl":"https://avatars.githubusercontent.com/u/65222?",
+                    "iconEnabled":true,"serviceName":"github"
+                }],
+                "typekitId":"wtl0dbz",
+                "statsMigrated":false,
+                "imageMetadataProcessingEnabled":false
+            },
+            "tzData":{"zones":[[60,"EU","CE%sT",null]],
+                "rules":{"EU":[[1981,"max",null,"Mar","lastSun","1:00u","1:00","S"],[1996,"max",null,"Oct","lastSun","1:00u","0",null]]}},"websiteCloneable":false,"isSystemPage":false,"collection":{"title":"Accueil","id":"523eeb4de4b061a78beb998b","fullUrl":"/","publicCommentCount":0},"demoCollections":[{"collectionId":"508475bee4b0f4598aa96c7f","deleted":true},
+            {"collectionId":"50d24f69e4b02dd4bbc64c7f","deleted":true},
+            {"collectionId":"50d24e97e4b0f1f627a34b01","deleted":true},
+            {"collectionId":"50d24edde4b063d94554668b","deleted":true},
+            {"collectionId":"51146d9ce4b0807f570d5d42","deleted":true},
+            {"collectionId":"51140633e4b07596654e9410","deleted":true},
+            {"collectionId":"51147155e4b099bd04f0327f","deleted":true},
+            {"collectionId":"51156265e4b0a31c035c55a4","deleted":true},
+            {"collectionId":"5159d1cce4b0ffd38a3ca92e","deleted":true}],
+            "subscribed":false,
+            "appDomain":"squarespace.com",
+            "templateId":"50996af1e4b0d2694881a49d",
+            "rollups":{"squarespace-audio-player":
+            {"css":"//static.squarespace.com/universal/styles-compressed/audio-player-63b580506e464538b6fe64025c4400bc-min.css","js":"//static.squarespace.com/universal/scripts-compressed/audio-player-12a132405b745c6219f6ef53309655e5-min.js"},
+                "squarespace-calendar-block-renderer":{"css":"//static.squarespace.com/universal/styles-compressed/calendar-block-renderer-d41d8cd98f00b204e9800998ecf8427e-min.css","js":"//static.squarespace.com/universal/scripts-compressed/calendar-block-renderer-9cccf56fa61e8d4e3996e4be340486ae-min.js"},"squarespace-comments":{"css":"//static.squarespace.com/universal/styles-compressed/comments-3ba19733672b0276f5d182e53d002c68-min.css","js":"//static.squarespace.com/universal/scripts-compressed/comments-f47fb713ebc91e26e4b2fbd8841a89ce-min.js"},"squarespace-dialog":{"css":"//static.squarespace.com/universal/styles-compressed/dialog-868631b0aa482d49923f373555fcfef9-min.css","js":"//static.squarespace.com/universal/scripts-compressed/dialog-83c93501d3554e03d2bfa41d6f73d7be-min.js"},"squarespace-pinterest":{"css":"//static.squarespace.com/universal/styles-compressed/pinterest-d41d8cd98f00b204e9800998ecf8427e-min.css","js":"//static.squarespace.com/universal/scripts-compressed/pinterest-d76b234c7c5f76cc23ce4b53b7bc843a-min.js"}},"tweakJSON":{"aspect-ratio":"Auto","gallery-arrow-style":"Round Corners","gallery-aspect-ratio":"3:2 Standard","gallery-auto-crop":"true","gallery-autoplay":"false","gallery-design":"Slideshow","gallery-info-overlay":"Always Show","gallery-loop":"false","gallery-navigation":"Thumbnails","gallery-show-arrows":"true","gallery-transitions":"Fade","galleryArrowBackground":"rgba(34,34,34,1)","galleryArrowColor":"rgba(255,255,255,1)","galleryAutoplaySpeed":"3","galleryCircleColor":"rgba(255,255,255,1)","galleryInfoBackground":"rgba(0, 0, 0, .7)","galleryThumbnailSize":"100px","gridSize":"350px","gridSpacing":"20px","product-gallery-auto-crop":"true","product-image-auto-crop":"false"},
+            "websiteSettings":
+            {"id":"523ee688e4b0534df7deb0bf",
+                "websiteId":"523ee688e4b0534df7deb0be",
+                "type":"Non-Profit","subject":"Software and IT",
+                "subjects":[{"systemSubject":"tech"}],"country":"FR","state":"","markdownMode":false,"simpleLikingEnabled":true,"lastAgreedTermsOfService":2,"defaultPostFormat":"%y/%m/%d/%t","commentLikesAllowed":true,"commentAnonAllowed":true,"commentThreaded":true,"commentApprovalRequired":false,"commentAvatarsOn":true,"commentSortType":2,"commentFlagThreshold":0,"commentFlagsAllowed":true,"commentEnableByDefault":true,"commentDisableAfterDaysDefault":0,"disqusShortname":"","notFoundPageId":"523eeb4de4b061a78beb998b","homepageTitleFormat":"%s","collectionTitleFormat":"%c \u2014 %s","itemTitleFormat":"%i \u2014 %s","commentsEnabled":true,"contactPhoneNumber":"+33 6 64 63 43 92","contactEmail":"inbox@simplon.co","allowSquarespacePromotion":true,"storeSettings":{"returnPolicy":null,"termsOfService":null,"privacyPolicy":null,"expressCheckout":false,"useLightCart":false,"showNoteField":false,"currenciesSupported":["USD"],"defaultCurrency":"USD","selectedCurrency":"USD","measurementStandard":1,"orderConfirmationInjectCode":"","showCustomCheckoutForm":false,"isLive":false,"stripeConnected":false,"storeState":3},"useEscapeKeyToLogin":true,"configurationMenuItems":[{},{},{},{},{},{},{}],"ssBadgeType":1,"ssBadgePosition":4,"ssBadgeVisibility":1,"ssBadgeDevices":1,"pinterestOverlayOptions":{"mode":"disabled","size":"small","shape":"rect","color":"white"}},"secureDomain":"https://simplonco.squarespace.com"};
+        SquarespaceFonts.loadViaContext();
+        Squarespace.load(window);
+    </script>
+
+    <script type="text/javascript" src="http://use.typekit.com/wtl0dbz.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+    <style>
+        #header #logo.logo-image {
+            position: relative;
+        }
+        #header #logo.logo-image img {
+            height: 150px;
+            float: left;
+            position: absolute;
+            top: 10px;
+            left: 20px;
+        }
+        #site-tagline h2 {
+            border: 1px solid red;
+            text-align: left;
+            float: left;
+            top: 114px;
+            left: 120px;
+            text-shadow: 0 1px 1px rgba(0,0,0,.8) !important;
+        }
+        #header {
+            position: relative !important;
+        }
+        .sqs-block.calendar-block .sqs-block-content, .sqs-block.calendar-block .sqs-block-content {
+            max-width: 100%;
+        }
+    </style>
+    <script type="text/javascript"> var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-39912989-1']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })(); </script><!-- End of Squarespace Headers -->
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+
+    <script type="text/javascript" src="https://static.squarespace.com/static/ta/508475bee4b0f4598aa96c7c/289/scripts/combo/?site.js"></script>
+
+
+
+    <link rel="stylesheet" href="assets/style.css" type="text/css" media="all">
+
+</head>
+
+<body>
+<div class="construction">Ce site est en construction - Merci de votre compréhension</div>
+
+<header id="header">
+    <div class="wrapper clear" data-content-field="page-settings">
+
+        <div class="primary-image default"></div>
+
+        <div class="primary-nav">
+
+            <ul class="nav clear" data-content-field="navigation-navigation">
+
+                <li class="page-collection"><a href="index.php" class="active">Accueil</a></li>
+                <li class="page-collection"><a href="" class="disabled">Pédagogie</a></li>
+
+                <li class="folder-wrapper">
+                    <a class="">Ecosystème</a>
+
+                    <ul class="folder-links-wrapper">
+                        <li class="page-collection"><a href="" class="">Comité de pilotage</a></li>
+                        <li class="page-collection"><a href="" class="">Partenaires</a></li>
+                        <li class="page-collection"><a href="" class="">Mentors</a></li>
+                    </ul>
+                </li>
+                <li class="folder-wrapper">
+                    <a class="">Actualités</a>
+
+                    <ul class="folder-links-wrapper">
+                        <li class="page-collection"><a href="" class="">blog</a></li>
+                    </ul>
+                </li>
+
+                <li class="page-collection"><a href="" class="">Candidatez</a></li>
+                <li class="page-collection"><a href="" class="">Devenez partenaire</a></li>
+                <li class="page-collection"><a href="" class="">Média</a></li>
+                <li class="page-collection"><a href="" class="">Contact</a></li>
+            </ul>
+
+
+        </div>
+
+
+        <div id="logo" class="logo-image">
+
+            <h1 class="site-title" data-content-field="site-title">
+                <div class="site-title-border"></div>
+                <a href="index.php">
+                    <img src="images/logo.png" style="width: 250px; height: inherit;" alt="Simplon.co" />
+                </a>
+            </h1>
+
+
+
+            <div class="site-tagline">
+                <div class="spacer"></div>
+                <h2>Fabrique de codeurs entrepreneurs dans le Perche #frenchtech</h2>
+            </div>
+
+
+        </div>
+
+
+        <div class="folder-nav">
+
+        </div>
+
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml" itemscope itemtype="http://schema.org/Thing" lang="fr-FR">
+
+<?php
+include('header.php');
+?>
+
+<div id="site">
+    <img src="images/paysage_st_victor_de_buthon_long_v3.jpg" alt="paysage" style="width: 100%;">
+    <div class="wrap clear">
+
+        <article>
+            <p>Installée au cœur du Parc naturel régional du Perche, l’école numérique Simplon Village propose <b>une
+                    formation gratuite et intensive de 6 mois au développement des applications web et mobile avec
+                    un accompagnement vers l’entrepreneuriat numérique, dans un cadre naturel propice à la
+                    créativité et à l’innovation.</b></p>
+            <p>Nous recrutons en priorité des talents défavorisés ou sous-représentés dans l’entrepreneuriat digital
+                et la programmation (femmes, personnes issues des zones rurales, des milieux modestes, seniors,
+                personnes en difficulté d’insertion professionnelle ou en situation de handicap...). L’objectif de la
+                formation est d’aider nos stagiaires à oser entreprendre et à intégrer efficacement le marché de
+                travail.</p>
+            <p>Simplon Village a été créé en réponse à l’appel à projets national lancé en mars 2014 par Simplon.co
+                et Zevillage pour la création des écoles numériques en milieu rural. Grâce à un engagement fort de la
+                Communauté de Communes des Portes du Perche et du Conseil Général d’Eure-et-Loir, ce projet a
+                pu voir le jour dans un temps record. La première session ouvrira ses portes dès le 1 er octobre 2014.
+                Le projet est porté par Innov’Educ, une entreprise sociale et solidaire spécialisée en éducation
+                innovante et implantée dans le Perche eurélien.</p>
+        </article>
+
+    </div>
+</div>
+
+
+
+<?php
+include('footer.php');
+?>
+
+
+
+</body>
+</html>
