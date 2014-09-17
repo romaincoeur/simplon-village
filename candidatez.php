2ff578c2c4e83ce2ac5a083be97a640772640ef0
romaincoeur <coeurro@gmail.com>
2014-09-17 12:13:51 +0200

    intégration modifs Anna suppression du lien vers le formulaire d'inscription

--- candidatez.php
+++ candidatez.php
@@ -10,8 +10,9 @@ include('header.php');
 
 <!--        <h1>Candidatez !</h1>-->
         <article>
-            <h3 style="margin-bottom: 40px;">Les inscriptions pour la première session 2014/2015 sont terminées. Les inscriptions pour la deuxième session (septembre 2015 - février 2016) seront
-                ouvertes en janvier 2015. Suivez-nous sur <a href="https://www.facebook.com/simplonvillage">Facebook</a> et <a href="https://twitter.com/SimplonVillage">Twitter</a> !</h3>
+            <h3 style="margin-bottom: 40px;">Les inscriptions pour la première session 2014/2015 sont terminées. Les inscriptions pour la deuxième session
+                (septembre 2015 - février 2016) seront ouvertes en janvier 2015. Suivez-nous sur <a href="https://www.facebook.com/simplonvillage">Facebook</a>
+                et <a href="https://twitter.com/SimplonVillage">Twitter</a> !</h3>
             <p><i>Vous voulez apprendre à faire des applications web, à créer une startup numérique ? Vous avez un
                 projet et le code vous permettrait de le réaliser ? Développez un projet entrepreneurial numérique
                 en apprenant la programmation par la pratique dans le cadre d’une formation intensive.</i></p>
@@ -26,26 +27,9 @@ include('header.php');
                     du profil et de la progression pédagogique, et qu'il est possible d'intégrer en cours de
                     promotion</li>
             </ul>
-            <p><a href="actualites.php">Venez nous rencontrer le samedi 28 juin lors de la Journée Portes Ouvertes à La Loupe : des
-                conférences et des animations tout au long de la journée autour des métiers et usages du
-                numérique</a></p>
             <p>Pour apprendre la programmation et pour intégrer Simplon Village, il faut être motivé et le
-                démontrer. Prenez donc le temps de bien préparer votre dossier de candidature.</p>
-            <p><b>Pour candidater, envoyez-nous votre CV et une lettre de motivation</b> décrivant bien votre projet :</p>
-            <ul>
-                <li>Pourquoi souhaitez-vous apprendre la programmation ?</li>
-                <li>Avez-vous un projet entrepreneurial qui nécessite les compétences numériques ? Si oui,
-                    lequel ?</li>
-                <li>Avez-vous déjà pratiqué l’informatique ? Si oui, dans quel cadre, qu’avez-vous pu réaliser ?
-                    (une connaissance préalable en programmation n’est pas obligatoire)</li>
-            </ul>
-            <p>Vous pouvez également joindre à votre dossier une présentation créative de vous-même ou de
-                votre projet (une vidéo, un dessin, un site internet, une application mobile, une prose littéraire...).</p>
-            <p><b>Afin d’être sûr que votre candidature sera examinée pour la première session (octobre 2014-mars
-                    2015), envoyez-nous votre dossier d’inscription avant le 30 juillet à <a href="mailto:simplon-village@gmail.com">simplon-village@gmail.com</a></b></p>
-            <p>Les candidats retenus sur dossiers seront sélectionnés à la suite d’un entretien et d’une journée de
-                mise en situation (une journée d’apprentissage de la programmation à l’école avant le début de la
-                session).</p>
+                démontrer. Prenez donc le temps de bien préparer votre dossier de candidature. Les inscriptions pour la deuxième session
+                (septembre 2015 - février 2016) seront ouvertes en janvier 2015.</p>
         </article>
     </div>
 </div>
